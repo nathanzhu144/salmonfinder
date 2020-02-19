18cccaf50a2605896dd9a3e68aed79b380a764a9
nathanzhu144 <znathan@umich.edu>
2020-02-19 12:52:55 -0500

    next

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+Setting up locally: 
+
+1. Make a file called "password.txt" in your home directory, note that .gitignore will ignore this file, and thus this file
+   should not be pushed up to git.
+2. Replace the email with a functional gmail, and put the password of that email in the "password.txt"
+3. Make sure signing on with less secure methods is turned on.
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from datetime import date
 from collections import defaultdict
 from get_requests import *
 from flask import Flask
-import smtplib
-import ssl
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 app = Flask(__name__)
 @app.route('/')
+
 def hello_world():
     """Print 'Hello, world!' as the response body."""
     return 'Hello, world!'
@@ -25,6 +27,11 @@ class DiningInfo:
         self.table = defaultdict(list)
         self.dining_h = ["east-quad", "bursley", "markley", "mosher-jordan", "north-quad", "south-quad", "twigs-at-oxford"]
         self.interesting_foods = interesting_foods_in
+
+    def get_html_bursley(self):
+        d = DiningInfo(interesting_foods)
+        raw_html = simple_get("https://dining.umich.edu/menus-locations/dining-halls/bursley")
+        print(BeautifulSoup(raw_html, 'html.parser').prettify())
 
     # Literally returns every menu item in a particular dining hall
     def find_all_foods_for_this_dining(self, dining_hall_nm):
@@ -49,7 +56,7 @@ class DiningInfo:
             all_food = self.find_all_foods_for_this_dining(dh)
             self.find_string_in_list(all_food, dh, self.interesting_foods)
                 
-    def print_table(self):
+    def get_table(self):
         ret = []
         for dh in self.table:
             for food in self.table[dh]:
@@ -60,38 +67,75 @@ class DiningInfo:
         return ret
             
 
+class Email:
+    def __init__(self, gmail):
+        self.gmail = gmail
+
+    def convert_content_to_html(self, content):
+        html = []
+        html.append("<html><body><p>")
+        for line in content: 
+            html.append(str(line))
+            html.append("<br>")
+        
+        html.append("</p></body></html>")
+        html = "".join(html)
+        return html
+
+    def send_email(self, html, recipient):
+        today = str(date)
+        me = self.gmail
+        my_password = str(open("password.txt").read())
+        you = recipient
+
+
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = "Food at dining halls on " + today
+        msg['From'] = me
+        msg['To'] = you
+
+        part2 = MIMEText(html, 'html')
+        msg.attach(part2)
+
+        # Send the message via gmail's regular server, over SSL - passwords are being sent, afterall
+        s = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        # uncomment if interested in the actual smtp conversation
+        # s.set_debuglevel(1)
+        # do the smtp auth; sends ehlo if it hasn't been sent already
+        s.login(me, my_password)
+        s.sendmail(me, you, msg.as_string())
+        s.quit()
+
+    def send_emails(self, content, emails):
+        c = self.convert_content_to_html(content)
+        for email in emails:
+            self.send_email(c, email)
+
+        
 
 if __name__ == "__main__":
     
     interesting_foods = ["salmon", "korean", "bbq", "steak"]
-    bad_foods = ["white fish", "catfish"]
     d = DiningInfo(interesting_foods)
-    # raw_html = simple_get("https://dining.umich.edu/menus-locations/dining-halls/bursley")
-    # print(BeautifulSoup(raw_html, 'html.parser').prettify())
     d.find_all_interesting_dishes()
-    d.print_table()
-
-    d2 = DiningInfo(bad_foods)
-    d2.find_all_interesting_dishes()
-    d2.print_table()
+    ret = d.get_table()
+    e = Email("nathanzhu12@gmail.com")
+    e.send_emails(ret, ["nathanzhu12@gmail.com", "znathan@umich.edu", "hsatam@umich.edu", "chenreny@umich.edu"])
 
 
+    # d2 = DiningInfo(bad_foods)
+    # d2.find_all_interesting_dishes()
+    # d2.print_table()
 
-    port = 465  # For SSL
-    smtp_server = "smtp.mail.yahoo.com"
-    sender_email = "dininghallapp@yahoo.com"  # Enter your address
-    receiver_email = "znathan@umich.edu"  # Enter receiver address
-    password = str(open("password.txt"))
-    message = """\
-    Subject: Hi there
 
-    This message is sent from Python."""
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+    
 
+
+
+
+
+    
     # app.run()
     # print(d.get_table())
     # today = date.today()
